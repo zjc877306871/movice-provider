ab65abad0930a4038d331d375e776c8298a2cb88
zhangjianchong <1204880901@qq.com>
2018-01-18 16:48:55 +0800

    Initial commit

---
+++ src/main/java/com/zhang/springcould/MoviceProviderApplication.java
@@ -0,0 +1,12 @@
+package com.zhang.springcould;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviceProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviceProviderApplication.class, args);
+	}
+}
